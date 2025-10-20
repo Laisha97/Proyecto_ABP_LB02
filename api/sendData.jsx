710f1bde03eb63ca780ff3487497364653a9c39f
Laisha <laisha.barronel@gmail.com>
2025-10-20 16:24:41 -0600

    Corrigiendo timeout en sendData

--- api/sendData.jsx
+++ api/sendData.jsx
@@ -4,20 +4,19 @@ export default async function handler(req, res) {
   }
 
   try {
-    const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxYIL_v5aV63F31RFthb47PgnoMHgOlnzrvbAknSWK5wqMqEhCBn5lUVaWCRKdIQef2/exec"
+    const { proyecto, tiempo, intentos } = JSON.parse(req.body)
 
-    const response = await fetch(GOOGLE_SCRIPT_URL, {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbxYIL_v5aV63F31RFthb47PgnoMHgOlnzrvbAknSWK5wqMqEhCBn5lUVaWCRKdIQef2/exec", {
       method: "POST",
+      body: JSON.stringify({ proyecto, tiempo, intentos }),
       headers: { "Content-Type": "application/json" },
-      body: req.body, 
-    });
+    })
 
-    const data = await response.text()
-    console.log("Respuesta de Google Script:", data)
+    const text = await response.text()
 
-    return res.status(200).json({ message: "Datos enviados a Google Sheets", data })
+    return res.status(200).json({ message: "Datos enviados correctamente", response: text })
   } catch (error) {
-    console.error("Error al enviar los datos:", error)
-    return res.status(500).json({ message: "Error al enviar datos" })
+    console.error(error)
+    return res.status(500).json({ message: "Error al enviar los datos", error })
   }
 }
