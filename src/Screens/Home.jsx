e020855750a80e19ee930245dcbbd3e6530067aa
Laisha <laisha.barronel@gmail.com>
2025-10-20 17:52:44 -0600

    Corrigiendo Home

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -5,13 +5,13 @@ import books from '../assets/books.png'
 import casa from '../assets/casa.png'
 import semaforo from '../assets/semaforo.png'
 import { Link as ScrollLink } from 'react-scroll'
-import { Link as RouterLink} from "react-router-dom"
+import { Link as RouterLink } from "react-router-dom"
 
 
 
 const Home = () => {
 
-    const [scrollY, setScrollY] = useState(0)
+  const [scrollY, setScrollY] = useState(0)
 
   useEffect(() => {
     const handleScroll = () => setScrollY(window.scrollY)
@@ -30,23 +30,23 @@ const Home = () => {
         <div className={styles.abpSection}>
           <div className={styles.abpText}>
             <div className={styles.textoContainer}>
-            <p className={styles['texto-abp']}>
-              <span className={styles.mini}>Educación digital</span>
-              <br></br>
-              <span className={styles.resaltado}>Aprendizaje Basado en Proyectos (ABP)</span> <br></br>
-              <br></br>
-              <span className={styles.Es}>Es una metodología educativa donde los estudiantes</span> 
-              <span className={styles.activo}> aprenden de manera activa</span> al planear, desarrollar y evaluar proyectos con aplicación en la vida real.  
-              <br /><br />
-              Con este enfoque, los alumnos se convierten en 
-              <span className={styles.protagonista}> protagonistas de su propio aprendizaje.</span>
-            </p>
+              <p className={styles['texto-abp']}>
+                <span className={styles.mini}>Educación digital</span>
+                <br></br>
+                <span className={styles.resaltado}>Aprendizaje Basado en Proyectos (ABP)</span> <br></br>
+                <br></br>
+                <span className={styles.Es}>Es una metodología educativa donde los estudiantes</span>
+                <span className={styles.activo}> aprenden de manera activa</span> al planear, desarrollar y evaluar proyectos con aplicación en la vida real.
+                <br /><br />
+                Con este enfoque, los alumnos se convierten en
+                <span className={styles.protagonista}> protagonistas de su propio aprendizaje.</span>
+              </p>
 
-        <RouterLink to="/Info" className={styles.info}>
-          Ver más información
-        </RouterLink>
-      </div>
-      </div>
+              <RouterLink to="/Info" className={styles.info}>
+                Ver más información
+              </RouterLink>
+            </div>
+          </div>
 
           {/* Imagen */}
           <div className={styles.abpbooks}>
@@ -55,52 +55,54 @@ const Home = () => {
         </div>
       </div>
 
-          {/* Sección de frase en movimiento */}
-          <div className={styles.sectionQuote}>
-            <p className={styles.quoteText}>
-              Aprende haciendo, crea tu futuro
-            </p>
-          </div>
+      {/* Sección de frase en movimiento */}
+      <div className={styles.sectionQuote}>
+        <p className={styles.quoteText}>
+          Aprende haciendo, crea tu futuro
+        </p>
+      </div>
 
-        {/* Segunda sección */}
-        <div id="projects" className={styles.section2}>
-          <div className={styles.textSide}>
+      {/* Segunda sección */}
+      <div id="projects" className={styles.section2}>
+        <div className={styles.textSide}>
           <h2 className={styles.segtitle}>Explora los proyectos</h2>
-            <p className={styles.projectsDescription}>
+          <p className={styles.projectsDescription}>
             Estos proyectos te permitirán aplicar el Aprendizaje Basado en Proyectos (ABP) y desarrollar habilidades prácticas mientras trabajas en retos reales.
-            </p>
-            </div>
+          </p>
+        </div>
 
-          <div className={styles.projectsContainer}>
-            
-            {/* Proyecto 1 */}
-            <div className={styles.projectCard}>
-              <div className={styles.projectImageContainer}>
-                <img src={casa} alt="Proyecto Casa" className={styles.projectImage} />
-              </div>
-              <div className={styles.projectTextContainer}>
+        <div className={styles.projectsContainer}>
+
+          {/* Proyecto 1 */}
+          <div className={styles.projectCard}>
+            <div className={styles.projectImageContainer}>
+              <img src={casa} alt="Proyecto Casa" className={styles.projectImage} />
+            </div>
+            <div className={styles.projectTextContainer}>
               <p className={styles.projectName}>Distribución del internet en casa</p>
               <RouterLink to="/proyecto1" className={styles.projectButton}>
                 Ir al reto
               </RouterLink>
             </div>
-            </div>
+          </div>
 
-            {/* Proyecto 2 */}
-            <div className={styles.projectCard}>
-              <div className={styles.projectImageContainer}>
-                <img src={semaforo} alt="Proyecto Semáforo" className={styles.projectImage} />
-              </div>
-              <div className={styles.projectTextContainer}>
+          {/* Proyecto 2 */}
+          <div className={styles.projectCard}>
+            <div className={styles.projectImageContainer}>
+              <img src={semaforo} alt="Proyecto Semáforo" className={styles.projectImage} />
+            </div>
+            <div className={styles.projectTextContainer}>
               <p className={styles.projectName}>Distribución de semáforos</p>
               <RouterLink to="/proyecto2" className={styles.projectButton}>
                 Ir al reto
-              </RouterLink>            
-              </div>
+              </RouterLink>
             </div>
           </div>
         </div>
       </div>
+
+      <p className={styles.nombre}>LVBE</p>
+    </div>
   )
 }
 
